12ec5163d3656f0500bbf27d5fed121bc4d147d1
Oleg Menyaylenko <oleg.menyaylenko@gmail.com>
2020-06-25 13:19:50 -0400

    test key

--- detroit_voter_backend/settings.py
+++ detroit_voter_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = "*m1povzyjkc#h+(s84ya8h68r2hxnu!)@u_!bg7l9=yqku=r6z"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
